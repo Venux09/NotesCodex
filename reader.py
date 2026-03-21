9df359e56e2fbb42ff4ffacb3d901aa403b3a71c
monu242 <monucut657@gmail.com>
2026-03-21 19:26:03 +0530

    called main.py func in reader file and then maked it  into text

---
+++ reader.py
@@ -0,0 +1,12 @@
+import pdfplumber
+from main import input
+
+
+def extract_text_from_pdf(input):
+    with pdfplumber.open(input) as pdf:
+        text = ""
+        for page in pdf.pages:
+            text += page.extract_text()
+            return text
+
+
