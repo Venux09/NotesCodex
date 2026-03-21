a57f801d03c58ba01320aec847b1a85ed886cfa6
monu242 <monucut657@gmail.com>
2026-03-21 19:25:17 +0530

    add . taked the pdf path as func

---
+++ main.py
@@ -0,0 +1,7 @@
+from reader import extract_text_from_pdf 
+
+def input():#calling the func so we can use the path and can generate the text in reader file
+     
+    pdf_path = input("Enter the path of the pdf file here to procced further : ")
+    return pdf_path
+
